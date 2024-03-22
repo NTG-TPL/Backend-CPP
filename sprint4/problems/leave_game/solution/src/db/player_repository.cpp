c3fde778790cd5efc0ce27ee126621746387b98a
NTG-TPL <nikolay.tep@yandex.ru>
2024-03-22 17:14:19 +0300

    delete final task

--- sprint4/problems/leave_game/solution/src/db/player_repository.cpp
+++
@@ -1,7 +0,0 @@
-#include "player_repository.h"
-#include <boost/uuid/uuid.hpp>
-#include <boost/uuid/random_generator.hpp>
-
-namespace app {
-
-}  // namespace domain
--- sprint4/problems/leave_game/solution/src/serialize.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "model_serialize/model_serialize.h"
-#include "app_serialize/ser_players.h"
